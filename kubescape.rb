a32e197a0154f8f439b5a1e37d90afd874f8c9bf
Avner Tzur <78496046+AvnerTzurArmo@users.noreply.github.com>
2021-10-10 09:53:24 +0300

    Update to release 109

--- kubescape.rb
+++ kubescape.rb
@@ -1,8 +1,8 @@
 class Kubescape < Formula
   desc "ARMO's kubescape is a tool for testing if Kubernetes is deployed securely"
   homepage "https://github.com/armosec/kubescape"
-  url "https://github.com/armosec/kubescape/releases/download/v1.0.102/kubescape-macos-latest"
-  sha256 "ac97ad4efe33bc511da8479b668eb398312025891ed6c1206773bd2106a246ff"
+  url "https://github.com/armosec/kubescape/releases/download/v1.0.109/kubescape-macos-latest"
+  sha256 "7e75f0f1e6026b5d3b2b94fd0ddcd0a3121b96c3af2e65dd002a1fa8b28cbe6b"
   license "Apache-2.0"
 
   def install
