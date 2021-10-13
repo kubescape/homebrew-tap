25049e1b0f12bd31de7173ee6deb08c4fc55e15a
Ferhat Yildiz <ferhat.yildiz@turingts.com>
2021-10-13 18:51:56 +0200

    upgrade to v1.0.117 and fix binary permissions

--- kubescape.rb
+++ kubescape.rb
@@ -1,11 +1,12 @@
 class Kubescape < Formula
   desc "ARMO's kubescape is a tool for testing if Kubernetes is deployed securely"
   homepage "https://github.com/armosec/kubescape"
-  url "https://github.com/armosec/kubescape/releases/download/v1.0.109/kubescape-macos-latest"
-  sha256 "7e75f0f1e6026b5d3b2b94fd0ddcd0a3121b96c3af2e65dd002a1fa8b28cbe6b"
+  url "https://github.com/armosec/kubescape/releases/download/v1.0.117/kubescape-macos-latest"
+  sha256 "8574cb24f964adb51223121aed77370f389309d1503fcee041b37894c89b5810"
   license "Apache-2.0"
 
   def install
+    system "chmod 555 kubescape-macos-latest"
     bin.install "kubescape-macos-latest" =>  "kubescape"
 
     output = Utils.safe_popen_read(bin/"kubescape", "completion", "bash")
