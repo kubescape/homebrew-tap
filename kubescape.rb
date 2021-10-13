6857df7a5b40d5add53149ada56a8cc85922ae06
Avner Tzur <atzur@cyberarmor.io>
2021-10-13 11:04:27 +0300

    Update kubescape version to 115

--- kubescape.rb
+++ kubescape.rb
@@ -1,8 +1,8 @@
 class Kubescape < Formula
-  desc "ARMO's kubescape is a tool for testing if Kubernetes is deployed securely"
+  desc "Kubernetes testing according to Hardening Guidance by NSA and CISA"
   homepage "https://github.com/armosec/kubescape"
-  url "https://github.com/armosec/kubescape/releases/download/v1.0.109/kubescape-macos-latest"
-  sha256 "7e75f0f1e6026b5d3b2b94fd0ddcd0a3121b96c3af2e65dd002a1fa8b28cbe6b"
+  url "https://github.com/armosec/kubescape/releases/download/v1.0.115/kubescape-macos-latest"
+  sha256 "75c7b244559a0f6d5491342687d18c9ccc662f9f3eff635a3b610cd9057e699a"
   license "Apache-2.0"
 
   def install
