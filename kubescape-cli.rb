c6e60b12c8971abcf043bbd5498d1464649d5956
Amir Malka <amirm@armosec.io>
2022-08-11 17:33:59 +0300

    Update kubescape-cli.rb

--- kubescape-cli.rb
+++ kubescape-cli.rb
@@ -6,15 +6,6 @@ class KubescapeCli < Formula
   license "Apache-2.0"
   head "https://github.com/armosec/kubescape.git", branch: "master"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "69ef7dc344edda8dc0cf2cebdf86169a63675fd0fbe08c9e57707add42839a3a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "264415412177372be6ce2e92673a7469115ff6ce6c29a6efec1eb2a733821695"
-    sha256 cellar: :any_skip_relocation, monterey:       "b33d66b12dbd461c7038a53fde23f027046ca30efe51bf66407abe96abf5d934"
-    sha256 cellar: :any_skip_relocation, big_sur:        "b38436d3d186c443b5deb669ac7be828296618d0f938dd68dc97340517d58f8b"
-    sha256 cellar: :any_skip_relocation, catalina:       "520774f9ca98acc82a4f86d2cc36e83684ad411f41bed5c262296647e1787b5d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "a8f380152c0c592450d46da3f3731853a452de6fe0a2fe824212465711ff6a9d"
-  end
-
   depends_on "cmake" => :build
   depends_on "go" => :build
   depends_on "pkg-config" => :build
