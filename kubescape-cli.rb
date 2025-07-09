ed6c3a21c0bfcd159ddbe64927e5caa15b7426d7
Jayant Pranjal <jayant.pranjal123@gmail.com>
2025-07-09 18:00:52 +0530

    use github api to get latest kubescape version
    Signed-off-by: Jayant Pranjal <jayant.pranjal123@gmail.com>

--- kubescape-cli.rb
+++ kubescape-cli.rb
@@ -1,8 +1,40 @@
 class KubescapeCli < Formula
   desc "Kubernetes misconfiguration testing"
   homepage "https://github.com/kubescape/kubescape"
-  url "https://github.com/kubescape/kubescape/archive/v3.0.34.tar.gz"
-  sha256 "1da5a3a7b1ef8f38569ee6277fa8cf9d747f9fe379c9283011c7464b7c1003de"
+  
+  # Fetch latest version from GitHub API
+  def self.latest_version_info
+    require "net/http"
+    require "json"
+    
+    uri = URI("https://api.github.com/repos/kubescape/kubescape/tags")
+    response = Net::HTTP.get_response(uri)
+    
+    if response.code == "200"
+      tags = JSON.parse(response.body)
+      version_tag = tags.find { |tag| tag["name"] =~ /^v\d+\.\d+\.\d+$/ }
+      if version_tag
+        version = version_tag["name"]
+        commit_sha = version_tag["commit"]["sha"]
+        
+        return {
+          version: version,
+          sha: commit_sha
+        }
+      end
+    end
+    
+    {
+      version: "v3.0.34",
+      sha: "1da5a3a7b1ef8f38569ee6277fa8cf9d747f9fe379c9283011c7464b7c1003de"
+    }
+  end
+  
+  version_info = latest_version_info
+  version_string = version_info[:version].sub(/^v/, "")
+  
+  url "https://github.com/kubescape/kubescape/archive/v#{version_string}.tar.gz"
+  sha256 :no_check
   license "Apache-2.0"
   head "https://github.com/kubescape/kubescape.git", branch: "master"
 
